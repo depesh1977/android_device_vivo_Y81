00143cd28f7c8ee7c112df1e352aced3fbfa5bd0
lopestorm <guillopes2002@gmail.com>
2024-09-11 19:54:03 -0300

    Update and rename omni_1808.mk to omni_PD1732F_EX.mk

--- omni_PD1732F_EX.mk
+++ omni_PD1732F_EX.mk
@@ -8,16 +8,21 @@ LOCAL_PATH := device/vivo/Y81
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_DEVICE := 1808
-PRODUCT_NAME := omni_1808
+# Inherit from PD1732F_EX device
+$(call inherit-product, device/vivo/PD1732F_EX/device.mk)
+
+PRODUCT_DEVICE := PD1732F_EX
+PRODUCT_NAME := omni_PD1732F_EX
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := vivo 1808
 PRODUCT_MANUFACTURER := vivo
+
+PRODUCT_GMS_CLIENTID_BASE := android-vivo-rev1
 
 PRODUCT_BOARD := k62v1_64_bsp
 
