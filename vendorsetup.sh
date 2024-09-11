816e4db0c5cf32a3a32e4ab8535cc468c9a08e2f
lopestorm <guillopes2002@gmail.com>
2024-09-11 19:57:37 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_PD1732F_EX-eng
 add_lunch_combo omni_PD1732F_EX-user
 add_lunch_combo omni_PD1732F_EX-userdebug
 add_lunch_combo omni_PD1732F_EX-eng
