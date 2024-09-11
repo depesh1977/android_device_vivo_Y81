d7bc8912d93f3fe2428d39e0eed22cdbde821e79
lopestorm <guillopes2002@gmail.com>
2024-09-11 17:23:38 -0300

    Update and rename omni_Y81.mk to omni_PD1732F_EX.mk

--- omni_PD1732F_EX.mk
+++ omni_PD1732F_EX.mk
@@ -12,9 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_DEVICE := Y81
-PRODUCT_NAME := omni_Y81
+PRODUCT_DEVICE := PD1732F_EX
+PRODUCT_NAME := omni_PD1732F_EX
 PRODUCT_BRAND := vivo
-PRODUCT_MODEL := Y81
+PRODUCT_MODEL := vivo 1808
 PRODUCT_MANUFACTURER := vivo
 
+PRODUCT_BOARD := k62v1_64_bsp
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=1808 \
+    PRODUCT_NAME=1808 \
+    PRIVATE_BUILD_DESC="full_k62v1_64_bsp-user 8.1.0 O11019 1625667558 release-keys"
+
+BUILD_FINGERPRINT := vivo/1808/1808:8.1.0/O11019/1625667558:user/release-keys
