fbb7fd4d19622a5def36892a79b056e6b28a83fe
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-09-19 14:14:56 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,10 @@ TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 
 # Encryption
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_FBE := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,barrier=0,data=ordered"
 
 # TWRP Debug Flags
 TARGET_USES_LOGD := true
