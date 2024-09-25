6530f1d351d731ec93e132866bf2b3a6ea1eafb4
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-09-25 21:28:13 +0300

    Update twrp_PD1732F_EX.mk

--- twrp_PD1732F_EX.mk
+++ twrp_PD1732F_EX.mk
@@ -7,7 +7,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from PD1732F_EX device
 $(call inherit-product, device/vivo/PD1732F_EX/device.mk)
