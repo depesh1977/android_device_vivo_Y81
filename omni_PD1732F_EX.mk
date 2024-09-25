2a39b20e2aaa3964b814a23a30f2082a508ab96a
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-09-25 21:18:58 +0300

    Update omni_PD1732F_EX.mk

--- omni_PD1732F_EX.mk
+++ omni_PD1732F_EX.mk
@@ -1,23 +1,19 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from PD1732F_EX device
 $(call inherit-product, device/vivo/PD1732F_EX/device.mk)
 
 PRODUCT_DEVICE := PD1732F_EX
-PRODUCT_NAME := omni_PD1732F_EX
+PRODUCT_NAME := twrp_PD1732F_EX
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := PD1732F_EX
 PRODUCT_MANUFACTURER := vivo
