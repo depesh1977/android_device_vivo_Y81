0a66d24cb0b922f2696008395ed77e3bb5cadf7f
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-09-29 19:40:51 +0300

    Update omni_PD1732F_EX.mk

--- omni_PD1732F_EX.mk
+++ omni_PD1732F_EX.mk
@@ -3,7 +3,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
