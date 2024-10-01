8ff27995bf19b3c3baaf21c8ab9db2e4cce7ee40
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-01 20:14:52 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,6 @@ TARGET_USES_MKE2FS := true
 TW_INCLUDE_NTFS_3G := true
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
-TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
 # Crypto
 #TW_INCLUDE_CRYPTO := true
